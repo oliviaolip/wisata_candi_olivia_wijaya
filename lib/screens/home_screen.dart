987a255dc3cb5dc9c427262ab2c7e55a1b85589d
oliviaolip <oliviaoliv2006@gmail.com>
2025-11-13 16:55:00 +0700

    Create HomeScreen widget with scaffold and TODOs
    Added HomeScreen widget with initial structure and TODOs.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+
+}
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // TODO: 1 Buat appbar dengan judul Wisata Candi
+      // TODO: 2 Buat body dengan GridView.builder
+      // TODO: 3 Buat ItemCard sebagai return value dari GridView.builder
+    );
+  }
+}
