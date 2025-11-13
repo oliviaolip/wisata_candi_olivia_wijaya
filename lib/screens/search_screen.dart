e4007c27bb3cc55c40b0b8724e930f3e460ff726
oliviaolip <oliviaoliv2006@gmail.com>
2025-11-13 16:53:22 +0700

    Create SearchScreen with Candi search feature
    Refactored ProfileScreen into SearchScreen with search functionality for Candi.

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,126 +1,150 @@
 import 'package:flutter/material.dart';
-import '/widgets/profile_info_item.dart';
+import '/models/candi.dart';
+import '/data/candi_data.dart';
+import '/screens/detail_screen.dart';
 
-class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
+
+class SearchScreen extends StatefulWidget{
+  const SearchScreen({super.key});
 
   @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
+  State<SearchScreen> createState() => _SearchScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen> {
-  bool isSignedIn = true;
-  String fullName = "";
-  String userName = "";
-  int favoriteCandiCount = 0;
-
-  void toggleSignIn() {
-    setState(() {
-      isSignedIn = !isSignedIn;
-    });
-  }
-  void signIn() => toggleSignIn();
-  void signOut() => toggleSignIn();
+class _SearchScreenState extends State<SearchScreen>{
+  //TODO 1: Deklarasikan variabel yang dibutuhkan
+  late List<Candi> _filteredCandi;
+  String _searchQuery = "";
+  late TextEditingController _searchController;
 
   @override
+  void initState() {
+    super.initState();
+    _filteredCandi = candiList;
+    _searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: 200,
-            width: double.infinity,
-            color: Colors.deepPurple,
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              children: [
-                // Header
-                Padding(
-                  padding: const EdgeInsets.only(top: 150),
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Stack(
-                      alignment: Alignment.bottomRight,
-                      children: [
-                        const CircleAvatar(
-                          radius: 60,
-                          backgroundImage: AssetImage('images/placeholder_image.png'),
-                        ),
-                        if (isSignedIn)
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.camera_alt),
-                            color: Colors.white,
-                          ),
-                      ],
-                    ),
+      // TODO 2: Buat appbar dengan judul pencarian candi
+        appBar: AppBar(title: Text('Pencarian Candi'),),
+        // TODO 3: Buat Body Berupa Column
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.deepPurple[50],
+                ),
+                child: TextField(
+                  autofocus: false,
+                  // TODO: 6. Implementasi fitur pencarian
+                  onChanged: (query) {
+                    setState(() {
+                      _searchQuery = query;
+                      if (query.isEmpty) {
+                        _filteredCandi = candiList;
+                      } else {
+                        _filteredCandi = candiList
+                            .where((candi) => candi.name
+                            .toLowerCase()
+                            .contains(query.toLowerCase()))
+                            .toList();
+                      }
+                    });
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Cari Candi...',
+                    prefixIcon: const Icon(Icons.search),
+                    border: InputBorder.none,
+                    // TODO: 7. Implementasi pengosongan input
+                    suffixIcon: _searchQuery.isNotEmpty
+                        ? IconButton(
+                      icon: const Icon(Icons.clear),
+                      onPressed: () {
+                        _searchController.clear();
+                        setState(() {
+                          _searchQuery = '';
+                          _filteredCandi = candiList;
+                        });
+                      },
+                    )
+                        : null,
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
                   ),
+                  controller: _searchController,
                 ),
-
-                const SizedBox(height: 24),
-                const Divider(color: Colors.grey),
-                const SizedBox(height: 4),
-
-                ProfileInfoItem(
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
-                  icon: Icons.lock,
-                  label: 'Pengguna',
-                  value: fullName,
-                  showEditIcon: false,
-                  onEditPressed: null,
-                  iconColor: Colors.amber,
-                ),
-                const SizedBox(height: 4),
-                const Divider(color: Colors.grey),
-                const SizedBox(height: 4),
-                
-                ProfileInfoItem(
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
-                  icon: Icons.person,
-                  label: 'Nama',
-                  value: userName,
-                  showEditIcon: isSignedIn,
-                  onEditPressed: isSignedIn ? () {} : null,
-                  iconColor: Colors.deepPurple,
-                ),
-                const SizedBox(height: 4),
-                const Divider(color: Colors.grey),
-                const SizedBox(height: 4),
-
-                ProfileInfoItem(
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
-                  icon: Icons.favorite,
-                  label: 'Favorit',
-                  value: favoriteCandiCount == 0
-                      ? '-'
-                      : '$favoriteCandiCount favorites',
-                  showEditIcon: false,
-                  onEditPressed: null,
-                  iconColor: Colors.redAccent,
-                ),
-
-                const SizedBox(height: 4),
-                const Divider(color: Colors.grey),
-                const SizedBox(height: 20),
-
-                TextButton(
-                  onPressed: isSignedIn ? signOut : signIn,
-                  child: Text(isSignedIn ? 'Sign Out' : 'Sign In'),
-                ),
-              ],
+              ),
             ),
-          ),
-        ],
-      ),
+            // TODO 5: Buat ListView Candi sebagai anak dari column
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: ListView.builder(
+                  itemCount: _filteredCandi.length,
+                  itemBuilder: (context, index) {
+                    final candi = _filteredCandi[index];
+                    return GestureDetector(
+                      onTap: () {
+                        // TODO: 8. Implementasi GestureDetector dan Hero animation
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailScreen(candi: candi),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              width: 100,
+                              height: 100,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Hero(
+                                  tag: 'candi-${candi.name}',
+                                  child: Image.asset(
+                                    candi.imageAsset,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    candi.name,
+                                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                                  ),
+                                  const SizedBox(height: 4),
+                                  Text(candi.location),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        )
     );
   }
 }
